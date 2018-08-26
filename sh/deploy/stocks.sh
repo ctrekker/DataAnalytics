3ddb6a694ece4b0d2d1386e0256f30b9951356ce
Connor Burns <ctrekker4@gmail.com>
2018-08-26 13:18:52 -0600

    #40 Training script completion

--- sh/deploy/stocks.sh
+++ sh/deploy/stocks.sh
@@ -6,8 +6,9 @@ mkdir tmp
 # Download newline seperated list of stock collections
 node sh/util/mdb mdb.json mdbcollections > tmp/collections.txt
 
-#DATE=`date +%Y-%m-%d`
-DATE="2018-08-24"
+DATE=`date +%Y-%m-%d`
+#DATE="2018-08-24"
+
 # Loop through each line of the file
 while read collection; do
   echo "$collection"
@@ -26,5 +27,10 @@ done <tmp/collections.txt
 
 # Clean out the input repository
 rm in/*
+# Export data out in zip format (TEMPORARY)
+zip -r data_$DATE.zip data
+rm data/*
 # Clean tmp directory
 rm -r tmp
+
+echo "Completed execution for $DATE"

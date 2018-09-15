045a6d3927e945c40ed95b290c99311b96f2eb76
Connor Burns <ctrekker4@gmail.com>
2018-09-14 23:23:40 -0600

    #75 WIP IntradayDownload prototype

--- IntradayDownload/src/main/java/com/burnscoding/stocks/IntradayDownloader.java
+++ IntradayDownload/src/main/java/com/burnscoding/stocks/IntradayDownloader.java
@@ -2,7 +2,11 @@ package com.burnscoding.stocks;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.InsertOneModel;
+import com.mongodb.client.model.WriteModel;
+import org.bson.Document;
 import pl.zankowski.iextrading4j.api.refdata.ExchangeSymbol;
 import pl.zankowski.iextrading4j.api.stocks.Chart;
 import pl.zankowski.iextrading4j.api.stocks.ChartRange;
@@ -11,8 +15,7 @@ import pl.zankowski.iextrading4j.client.rest.request.refdata.SymbolsRequestBuild
 import pl.zankowski.iextrading4j.client.rest.request.stocks.ChartRequestBuilder;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class IntradayDownloader {
     private static String API_BASE_URL = "https://api.iextrading.com/1.0";
@@ -26,15 +29,49 @@ public class IntradayDownloader {
     public IntradayDownloader() {
         IEXTradingClient tradingClient = IEXTradingClient.create();
         List<ExchangeSymbol> symbolList = tradingClient.executeRequest(new SymbolsRequestBuilder().build());
+        int symbolNumber = 0;
         for(ExchangeSymbol symbol : symbolList) {
             List<Chart> chartList = tradingClient.executeRequest(new ChartRequestBuilder()
                     .withChartRange(ChartRange.INTRADAY)
                     .withSymbol(symbol.getSymbol())
                     .build());
+            List<WriteModel<Document>> entryDocuments = new ArrayList<WriteModel<Document>>();
             for(Chart entry : chartList) {
                 if(entry.getMarketAverage()==null||entry.getMarketAverage().equals(new BigDecimal(-1))) continue;
-                //System.out.println(entry.getMarketClose());
+                //System.out.println(entry.getMinute());
+
+                String s_year = entry.getDate().substring(0, 4);
+                String s_month = entry.getDate().substring(4, 6);
+                String s_day = entry.getDate().substring(6, 8);
+                int year = Integer.parseInt(s_year);
+                int month = Integer.parseInt(s_month);
+                int day = Integer.parseInt(s_day);
+                String formattedDate = s_year + "-" + s_month + "-" + s_day;
+
+                String s_hour = entry.getMinute().substring(0, 2);
+                String s_minute = entry.getMinute().substring(3, 5);
+                int hour = Integer.parseInt(s_hour);
+                int minute = Integer.parseInt(s_minute);
+                String formattedTime = s_hour + ":" + s_minute + ":00";
+                //System.out.println(formattedTime);
+
+                entryDocuments.add(new InsertOneModel<>(new Document("timestamp", new GregorianCalendar(year, month-1, day, hour, minute).getTime())
+                        .append("date", formattedDate)
+                        .append("time", formattedTime)
+                        .append("open", entry.getMarketOpen())
+                        .append("high", entry.getMarketHigh())
+                        .append("low", entry.getMarketLow())
+                        .append("close", entry.getMarketClose())
+                        .append("volume", entry.getMarketVolume())));
+                //System.out.println(entryDocuments.get(entryDocuments.size()-1));
             }
+            MongoCollection<Document> dbCollection = db.getCollection(symbol.getSymbol());
+            if(entryDocuments.size()>0) {
+                dbCollection.bulkWrite(entryDocuments);
+            }
+
+            symbolNumber++;
+            System.out.println("Finished symbol "+symbolNumber+" / "+symbolList.size());
         }
 
 //        StockIndex index = StockIndex.fromUrl(API_BASE_URL + "/ref-data/symbols");
